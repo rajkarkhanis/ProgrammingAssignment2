3158edc10cff00d17543bc974be71dedcfbcdedf
raj.karkhanis <rkarkhanis00@gmail.com>
2021-02-26 13:15:04 +0530

    Completed week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,46 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+# This function creates a "special" matrix,
+# with x as the matrix and inv as the inverse of the matrix
+# get and set are the getters and setters for the matrix x
+# getInv and setInv are the getters and setters for inverse of matrix x
 
+# The list created at the end allows for the $ operator extraction
+# x$get will extract the matrix and x$getInv will extract the inverse
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInv <- function(inverse) inv <<- inverse
+  getInv <- function() inv
+  
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
+# First check if the inverse is already calculated. if it isn't,
+# calculate the matrix with the built in solve() function.
+# Finally, set the calculated inverse using x$setInv
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  
+  if(!is.null(inv)){
+    message("Getting cached inverse")
+    return(inv)
+  }
+  
+  matrix <- x$get
+  inv <- solve(matrix, ...)
+  x$setInv(inv)
+  inv
 }
